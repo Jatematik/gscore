97b694e63df70f6f07347aa74fbce24c41d33ac3
Dmitry Yachkurinsky <jatematik@gmail.com>
2022-05-19 17:11:20 +0600

    feat: created redirect if unauthorized

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,6 +1,8 @@
-import { NextPage } from "next";
-import React from "react";
-import styled, { css } from "styled-components";
+import { NextPage, GetStaticProps, GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { css } from "styled-components";
+import * as cookie from "cookie";
 
 import { MainLayout } from "src/layouts/MainLayout";
 import { ITitle } from "src/ui/ITitle";
@@ -8,8 +10,20 @@ import { Tabs } from "src/components/Tabs";
 import { PersonalInfoForm } from "src/components/PersonalInfoForm";
 import { ChangePasswordForm } from "src/components/ChangePasswordForm";
 import Container from "src/layouts/Container/Container";
+import { useAppSelector } from "src/store/hooks";
+import { selectors } from "src/store/ducks";
+import { routes } from "src/types/routes";
 
 const SettingsPage: NextPage = () => {
+  const router = useRouter();
+  const token = useAppSelector(selectors.user.selectToken);
+
+  useEffect(() => {
+    if (!token) {
+      router.push(routes.LOGIN);
+    }
+  }, [token, router]);
+
   return (
     <MainLayout title="Gscore | Setting">
       <Container containerStyles={containerStyles}>
@@ -32,6 +46,31 @@ const SettingsPage: NextPage = () => {
 };
 
 export default SettingsPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (req.headers.cookie) {
+    const parsedCookies = cookie.parse(req.headers.cookie);
+
+    if (parsedCookies.token === "") {
+      return {
+        redirect: {
+          destination: routes.LOGIN,
+          statusCode: 302,
+        },
+      };
+    }
+    return {
+      props: {},
+    };
+  } else {
+    return {
+      redirect: {
+        destination: routes.LOGIN,
+        statusCode: 302,
+      },
+    };
+  }
+};
 
 const containerStyles = css`
   padding: 32px 0px;
--- pages/subscriptions/index.tsx
+++ pages/subscriptions/index.tsx
@@ -1,5 +1,8 @@
-import { NextPage } from "next";
+import { useEffect } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
 import { css } from "styled-components";
+import * as cookie from "cookie";
 
 import Container from "src/layouts/Container/Container";
 import { MainLayout } from "src/layouts/MainLayout";
@@ -7,8 +10,20 @@ import { ISwiper } from "src/ui/ISwiper";
 import { ITitle } from "src/ui/ITitle";
 import { IButton } from "src/ui/IButton";
 import { CardItem } from "src/components/CardItem";
+import { routes } from "src/types/routes";
+import { useAppSelector } from "src/store/hooks";
+import { selectors } from "src/store/ducks";
 
 const Subscriptions: NextPage = () => {
+  const router = useRouter();
+  const token = useAppSelector(selectors.user.selectToken);
+
+  useEffect(() => {
+    if (!token) {
+      router.push(routes.LOGIN);
+    }
+  }, [token, router]);
+
   return (
     <MainLayout title="Gscore | Subscriptions">
       <Container containerStyles={containerStyles}>
@@ -26,6 +41,31 @@ const Subscriptions: NextPage = () => {
 };
 
 export default Subscriptions;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (req.headers.cookie) {
+    const parsedCookies = cookie.parse(req.headers.cookie);
+
+    if (parsedCookies.token === "") {
+      return {
+        redirect: {
+          destination: routes.LOGIN,
+          statusCode: 302,
+        },
+      };
+    }
+    return {
+      props: {},
+    };
+  } else {
+    return {
+      redirect: {
+        destination: routes.LOGIN,
+        statusCode: 302,
+      },
+    };
+  }
+};
 
 const titleStyles = css`
   text-align: start;
